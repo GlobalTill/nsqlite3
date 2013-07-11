5c03be94bedc0353f571fd222cba27b88bfca644
M MacKinnon <mike@globaltill.com>
2013-07-10 21:07:23 -0400

    drop the tar.gz dependency and use python instead - refs #151

---
+++ deps/extract.py
@@ -0,0 +1,8 @@
+import sys
+import tarfile
+import os
+
+tarball = sys.argv[1]
+
+if not os.path.exists(tarball.replace('.tar.gz',Name:M MacKinnon <mike@globaltill.com>')):
+    tarfile.open().extractall('.')
